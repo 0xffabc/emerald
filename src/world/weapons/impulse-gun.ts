226f66224de2e277cb8660a84cc4b77c84851366
0xffabc <139798156+0xffabc@users.noreply.github.com>
2026-02-10 19:16:21 +0500

    [Ref] Remove dead code

--- src/world/weapons/impulse-gun.ts
+++ src/world/weapons/impulse-gun.ts
@@ -1,5 +1,4 @@
 import { Weapons } from "../../packet/constants/weapons";
-import { Serializer } from "../../packet/serialize/serialize";
 import { Weapon } from "../objects/weapon";
 
 export class ImpulseGun extends Weapon {
@@ -9,19 +8,6 @@ export class ImpulseGun extends Weapon {
 
   public override get type(): number {
     return 2;
-  }
-
-  public intermediateProcess(packet: Serializer): void {
-    /*packet.string("animation");
-
-    packet.raw([68]);
-    packet.integerU32(2, false);
-
-    packet.string("state");
-    packet.string("Idle");
-    packet.string("timeStamp");
-
-    packet.integerU32(1183055080);*/
   }
 
   public get id() {
